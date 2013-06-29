b4b9d1959a98d5fe0e3491aa55a8be28469b1e99
Andrei Lupu <andreilupu>
2013-06-29 12:27:11 +0300

    fix undefinded variables

--- params/slider.php
+++ params/slider.php
@@ -1,7 +1,9 @@
 <?php
 $class= "span12";
 
-if ( isset($param['admin_class'] ) ) $class = $param['admin_class']; ?>
+if ( isset($param['admin_class'] ) ) $class = $param['admin_class'];
+$is_content = '';
+if ( isset($param['is_content'] ) ) $is_content = 'class="is_shortcode_content"';  ?>
 
 <div class="wpgrade_grid_row <?php echo $class; ?>" >
     <label for="<?php echo $param['param_key'] ?>"><?php echo $param['name'] ?></label>
