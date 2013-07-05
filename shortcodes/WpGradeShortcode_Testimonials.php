5a7dc2292a114ec1b3f93bf7fb581d36cdb1438d
Andrei Lupu <andreilupu>
2013-07-05 14:24:31 +0300

    ++

--- shortcodes/WpGradeShortcode_Testimonials.php
+++ shortcodes/WpGradeShortcode_Testimonials.php
@@ -82,7 +82,6 @@ class WpGradeShortcode_Testimonials extends  WpGradeShortcode {
         //     $include_array = explode( ',', $include );
         //     $query_args['posts__in'] = $include_array;
         // }
-
         // if ( !empty( $exclude ) ) {
         //     $exclude_array = explode( ',', $exclude );
         //     $query_args['post__not_in'] = $exclude_array;
@@ -92,36 +91,36 @@ class WpGradeShortcode_Testimonials extends  WpGradeShortcode {
 
         if ( $query-> have_posts() ) : ?>
             <div class="testimonials_slide wp_slider">
-            <ul class="slides">
-            <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-                <li class="slide">
-                    <?php
-                        $author_name = get_post_meta(get_the_ID(), '_wpgrade_author_name', true);
-                        $author_function = get_post_meta(get_the_ID(), '_wpgrade_author_function', true);
-                        $author_link = get_post_meta(get_the_ID(), '_wpgrade_author_link', true);
-                    ?>
-                    <blockquote>
-                        <div class="testimonial_content"><?php the_content(); ?></div>
-                        <div class="testimonial_author">
+                <ul class="slides">
+                    <?php while ( $query->have_posts() ) : $query->the_post(); ?>
+                        <li class="slide">
+                            <?php
+                            $author_name = get_post_meta(get_the_ID(), '_wpgrade_author_name', true);
+                            $author_function = get_post_meta(get_the_ID(), '_wpgrade_author_function', true);
+                            $author_link = get_post_meta(get_the_ID(), '_wpgrade_author_link', true);
+                            ?>
+                            <blockquote>
+                                <div class="testimonial_content"><?php the_content(); ?></div>
+                                <div class="testimonial_author">
 
-                        <?php if(!empty($author_link)) { ?>
-                            <a href="<?php echo $author_link; ?>">
-                        <?php }
-                            if ( !empty($author_name)) { ?>
-                            <span class="author_name"><?php echo $author_name; ?></span>
-                        <?php }
-                            if ( !empty($author_function) ) {?>
-                             , <span class="author_function"><?php echo $author_function; ?></span>
-                        <?php }
-                            if(!empty($author_link)) { ?>
-                           </a>
-                        <?php } ?>
+                                <?php if(!empty($author_link)) { ?>
+                                    <a href="<?php echo $author_link; ?>">
+                                <?php }
+                                    if ( !empty($author_name)) { ?>
+                                    <span class="author_name"><?php echo $author_name; ?></span>
+                                <?php }
+                                    if ( !empty($author_function) ) {?>
+                                     , <span class="author_function"><?php echo $author_function; ?></span>
+                                <?php }
+                                    if(!empty($author_link)) { ?>
+                                   </a>
+                                <?php } ?>
 
-                        </div>
-                    </blockquote>
-                </li>
-            <?php endwhile;?>
-            </ul>
+                                </div>
+                            </blockquote>
+                        </li>
+                    <?php endwhile;?>
+                </ul>
             </div>
         <?php endif; wp_reset_query();
         return ob_get_clean();
