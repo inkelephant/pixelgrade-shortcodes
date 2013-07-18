9e7a075d3b5995cafe2c02f6ee5c8ca019cf559d
Vlad Olaru <vlad.olaru@pixelgrade.com>
2013-07-18 18:55:02 +0300

    changed to version 1.6.2

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Pixelgrade Shortcodes
 Plugin URI: http://pixelgrade.com
 Description: Adds shortcodes to your wordpress editor
-Version: 1.6.1
+Version: 1.6.2
 Author: Pixelgrade Media
 Author URI: http://pixelgrade.com
 Author Email: contact@pixelgrade.com
