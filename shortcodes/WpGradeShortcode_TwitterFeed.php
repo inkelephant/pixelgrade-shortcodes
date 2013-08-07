c3f07c670d855c53c6154b8b9b15b585f9073e74
cristianfrumusanu <cristian.frumusanu@gmail.com>
2013-08-07 12:17:01 +0300

    modificare mark-up

--- shortcodes/WpGradeShortcode_TwitterFeed.php
+++ shortcodes/WpGradeShortcode_TwitterFeed.php
@@ -81,18 +81,20 @@ class WpGradeShortcode_TwitterFeed extends  WpGradeShortcode {
 		
         if ( $results ){
 //        	$content .= '<div class="twitter-icon"><i class="icon-twitter"></i></div>';
+            $content .= '<div class="twitter-shortcode-tweets_container">';
             $content .= '<ul class="twitter-shortcode-tweets slides '.$class.'">';
             foreach ($results as $key => $result) {
                 $content .= '<li class="twitter-shortcode-tweet slide">
-                    <div class="twitter-shortcode-tweet-content">'.$this->get_parsed_tweet($result) .'</div>
                     	<div class="twitter-shortcode-tweet-meta">
                         <span class="twitter-shortcode-screenname">' . ucwords($config['screenname']) . '</span>' .
                         '<span class="twitter-shortcode-username"><a href="'.$link.'">@' . $config['screenname'] . '</a></span>' .
-                        '<span class="twitter-shortcode-tweet-date">' . $this->convert_twitter_date($result["created_at"]) . '</span></div>                 
+                        '<span class="twitter-shortcode-tweet-date">' . $this->convert_twitter_date($result["created_at"]) . '</span></div>
+                         <div class="twitter-shortcode-tweet-content">'.$this->get_parsed_tweet($result) .'</div>                       
                 </li>';
             
             }
             $content .= '</ul>';
+            $content .= '</div>';
         }
 		
 		return $content;
