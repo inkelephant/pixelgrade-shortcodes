3598d57f5a9c084d01bd7b5c3f80b1fce976889d
Andrei Lupu <andreilupu>
2013-08-13 15:37:34 +0300

    update to version 1.6.9

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 pixelgrade-shortcodes
 ==================
 
-~Current Version:1.6.8~
+~Current Version:1.6.9~
 
 ==================
 
--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Pixelgrade Shortcodes
 Plugin URI: http://pixelgrade.com
 Description: Adds shortcodes to your wordpress editor
-Version: 1.6.8
+Version: 1.6.9
 Author: Pixelgrade Media
 Author URI: http://pixelgrade.com
 Author Email: contact@pixelgrade.com
